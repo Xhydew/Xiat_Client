8a564b639fc0b1d3699be208ea0575b96b3dc120
Xhydew <sidewinder476@gmail.com>
2015-11-17 18:18:20 +0100

    Manda recibido por teclado

---
+++ src/xiat_client/Xiat_Client.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xiat_aclientados;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Joan Galiana MagÃ¡n
+ */
+public class Client {
+
+    private static final int puerto = 5050;
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+
+        Scanner teclao = new Scanner(System.in);
+
+        Socket cliSoc = new Socket("127.0.0.1", puerto);
+
+        Scanner dentro = new Scanner(cliSoc.getInputStream());
+        PrintWriter fuera = new PrintWriter(cliSoc.getOutputStream());
+        String env;
+        System.out.println("Con");
+        do {
+            env = teclao.nextLine();
+            fuera.println(env);
+            System.err.println("Enviando " + env);
+            fuera.flush();
+        } while (!env.equals("0"));
+    }
+
+}
